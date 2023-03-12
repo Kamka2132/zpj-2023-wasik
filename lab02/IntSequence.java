80276e4bb7e4094c6986c8f5dcbe3dc7d4834509
kamila <k_w@onet.pl>
2023-03-12 17:27:44 +0100

    zad-lab2

---
+++ lab02/IntSequence.java
@@ -0,0 +1,52 @@
+package lab02;
+
+public class IntSequence {
+    private static int[] sekwencja;
+
+    private IntSequence(int[] sekwencja){
+        this.sekwencja = sekwencja;
+    }
+
+    public static IntSequence of (int... wartosci){
+        return new IntSequence(wartosci);
+    }
+
+        // wewnetrzna klasa anonimowa
+    // public static IntSequence of (int... wartosci) {
+    //     return new IntSequence (new int[] {wartosci}) {
+    //         public int suma() {
+    //             int suma = 0;
+    //             for (int i = 0; i < sekwencja.length; i++) {
+    //                 suma += sekwencja[i];
+    //             }
+    //             return suma;
+    //         }
+    //     };
+    // }
+
+    public int[] getSequence() {
+        return sekwencja;
+    }
+
+    public String toString(){
+        StringBuilder napis = new StringBuilder();
+        napis.append("IntSequence -> [");
+        for (int i = 0; i < sekwencja.length; i++){
+            napis.append(sekwencja[i]);
+            if ( i < sekwencja.length - 1){
+                napis.append(", ");
+            }
+        }
+        napis.append("]");
+        return napis.toString();
+    }
+
+    public static IntSequence constant(int liczba){
+        while(true){
+            System.out.println(liczba);
+        }
+    }
+
+    
+    
+}
